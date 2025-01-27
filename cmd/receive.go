2ee19b15f329b8077fb12d90a1d96fd16ef5887e
LinusOP <32105232+LinusOP@users.noreply.github.com>
2025-01-27 15:56:21 +0100

    Simple TCP server/client with basic logging

--- cmd/receive.go
+++ cmd/receive.go
@@ -2,6 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"net"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -12,6 +16,41 @@ var receiveCmd = &cobra.Command{
 	Short:   "Sets this PC up as a receiver",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("This PC is a receiver")
+
+		listener, err := net.Listen("tcp", ":34567")
+
+		if err != nil {
+			fmt.Printf("Error occurred when setting up listener: %s", err.Error())
+			os.Exit(1)
+		}
+
+		defer listener.Close()
+		fmt.Println("Listening on port 34567")
+
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Printf("Error occurred when accepting connection: %s", err.Error())
+			os.Exit(1)
+		}
+
+		defer conn.Close()
+		buffer := make([]byte, 1024)
+
+		for {
+			n, err := conn.Read(buffer)
+
+			if err != nil {
+				if err == io.EOF {
+					fmt.Println("Received EOF")
+					os.Exit(0)
+				} else {
+					fmt.Printf("Error occurred when reading data: %s", err.Error())
+					os.Exit(1)
+				}
+			}
+
+			fmt.Printf("Received %s at %d\n", buffer[:n], time.Now().Local().UnixMilli()/100)
+		}
 	},
 }
 
--- cmd/send.go
+++ cmd/send.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"net"
+	"os"
 	"regexp"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -18,7 +21,7 @@ var sendCmd = &cobra.Command{
 
 		ip := args[0]
 
-		if match, _ := regexp.MatchString("^[1-9]{1,3}\\.[1-9]{1,3}\\.[1-9]{1,3}\\.[1-9]{1,3}$", ip); match {
+		if match, _ := regexp.MatchString("^[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}$", ip); match {
 			return nil
 		}
 
@@ -26,6 +29,44 @@ var sendCmd = &cobra.Command{
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("This PC is a sender to: %s\n", args[0])
+
+		conn, err := net.Dial("tcp", args[0]+":34567")
+		if err != nil {
+			fmt.Printf("Error occurred when setting up connection: %s", err.Error())
+			os.Exit(1)
+		}
+
+		defer conn.Close()
+
+		fmt.Println("Sending initial HELLO")
+		data := []byte("HELLO")
+		_, err = conn.Write(data)
+
+		if err != nil {
+			fmt.Printf("Error occurred when writing data: %s", err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println("Starting continous ping in")
+		fmt.Println("3")
+		time.Sleep(1 * time.Second)
+		fmt.Println("2")
+		time.Sleep(1 * time.Second)
+		fmt.Println("1")
+		time.Sleep(1 * time.Second)
+		fmt.Println("Starting continous ping")
+
+		for {
+			data := []byte("PING")
+			_, err = conn.Write(data)
+
+			if err != nil {
+				fmt.Printf("Error occurred when writing data: %s", err.Error())
+				os.Exit(1)
+			}
+
+			time.Sleep(100 * time.Millisecond)
+		}
 	},
 }
 
